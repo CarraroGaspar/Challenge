01d5d6bd85df271a543190ae3d7b571d73239537
Gaspar Carraro <gaspar.carraro@gmail.com>
2021-06-20 17:16:53 -0300

    Commit inicial

---
+++ COABackendChallenge/COABackendChallengeContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using COABackendChallenge.Models;
+
+namespace COABackendChallenge
+{
+    public class COABackendChallengeContext : DbContext
+    {
+        public COABackendChallengeContext (DbContextOptions<COABackendChallengeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Usuario> Usuario { get; set; }
+    }
+}
---
+++ COABackendChallenge/Controllers/UsuariosController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using COABackendChallenge.Models;
+
+namespace COABackendChallenge.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        private readonly COABackendChallengeContext _context;
+
+        public UsuariosController(COABackendChallengeContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Usuarios
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Usuario>>> GetUsuario()
+        {
+            return await _context.Usuario.ToListAsync();
+        }
+
+        // GET: api/Usuarios/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Usuario>> GetUsuario(long id)
+        {
+            var usuario = await _context.Usuario.FindAsync(id);
+
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return usuario;
+        }
+
+        // PUT: api/Usuarios/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Usuario>> PutUsuario(long id, [Bind("Nombre, Email, Telefono")] Usuario usuario)
+        {
+            usuario.Id = id;
+
+            _context.Entry(usuario).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UsuarioExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return usuario;
+        }
+
+        // POST: api/Usuarios
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Usuario>> PostUsuario(Usuario usuario)
+        {
+            _context.Usuario.Add(usuario);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (UsuarioExists(usuario.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetUsuario", new { id = usuario.Id }, usuario);
+        }
+
+        // DELETE: api/Usuarios/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Usuario>> DeleteUsuario(long id)
+        {
+            var usuario = await _context.Usuario.FindAsync(id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            _context.Usuario.Remove(usuario);
+            await _context.SaveChangesAsync();
+
+            return usuario;
+        }
+
+        private bool UsuarioExists(long id)
+        {
+            return _context.Usuario.Any(e => e.Id == id);
+        }
+    }
+}
